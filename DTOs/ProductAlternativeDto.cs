f483c69bb91320550eb732e9237b0c86818866af
Nariman Mohamed <116460166+nariman26@users.noreply.github.com>
2026-02-25 02:03:58 +0200

    Add files via upload

---
+++ DTOs/ProductAlternativeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SalamatyAPI.Dtos.Products
+{
+    public class ProductAlternativeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public string ImageUrl { get; set; } = default!;
+    }
+}
