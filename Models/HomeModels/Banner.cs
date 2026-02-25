8a88d1f4cbf589a3ae07fd7448b376d460d09068
ns1839-netizen <ns1839@fayoum.edu.eg>
2026-02-25 19:32:41 +0200

    Banner

--- Models/HomeModels/Banner.cs
+++ Models/HomeModels/Banner.cs
@@ -3,7 +3,9 @@
     public class Banner
     {
         public int Id { get; set; }
-        public string? ImageUrl { get; set; }
-        public string? Link { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty; // ده اللي هيشيل مسار الصورة زي /banners/Diabetes.jpg
+        public string DetailsUrl { get; set; } = string.Empty;
     }
 }
