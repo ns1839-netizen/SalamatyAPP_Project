8ca39de39cede468f2f4b1311c5690e619f9ed59
Nariman Mohamed <116460166+nariman26@users.noreply.github.com>
2026-02-25 02:10:25 +0200

    Add files via upload

---
+++ Models/Favorite.cs
@@ -0,0 +1,11 @@
+﻿namespace SalamatyAPI.Models
+{
+    public class Favorite
+    {
+        public int FavoriteId { get; set; }
+        public int UserId { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = default!;
+    }
+}
