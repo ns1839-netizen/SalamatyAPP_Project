94e881bd40565543d56e51feed41a78749fb7697
ns1839-netizen <ns1839@fayoum.edu.eg>
2026-03-02 18:04:15 +0200

    Update InsuranceProfile.cs

--- Models/InsuranceProfile.cs
+++ Models/InsuranceProfile.cs
@@ -1,17 +1,20 @@
-namespace SalamatyAPI.Models
+﻿using Salamaty.API.Models.ProfileModels;
+using SalamatyAPI.Models;
+
+public class InsuranceProfile
 {
-    public class InsuranceProfile
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public int InsuranceProviderId { get; set; }
-        public string CardHolderId { get; set; } = null!;
-        //public DateTime? ValidUntil { get; set; }
-        public string FrontImagePath { get; set; } = null!;
-        public string BackImagePath { get; set; } = null!;
-        public User User { get; set; } = null!;
-        public InsuranceProvider InsuranceProvider { get; set; } = null!;
-    }
+    public int Id { get; set; }
+
+    // تأكدي أن الـ UserId نوعه string
+    public string UserId { get; set; } = null!;
+
+    // التعديل المهم: الربط بـ ApplicationUser
+    public virtual ApplicationUser User { get; set; } = null!;
+
+    public int InsuranceProviderId { get; set; }
+    public virtual InsuranceProvider InsuranceProvider { get; set; } = null!;
+
+    public string CardHolderId { get; set; } = null!;
+    public string FrontImagePath { get; set; } = null!;
+    public string BackImagePath { get; set; } = null!;
 }
-
-
