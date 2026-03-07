9068136c933254a5457bb55003433d71c966134b
Nariman Mohamed <116460166+nariman26@users.noreply.github.com>
2026-03-07 16:42:19 +0200

    Delete Models directory

--- Models/Enums/Gender.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Salamaty.API.Models.Enums // تأكدي من ضبط الـ Namespace حسب مشروعك
-{
-    public enum Gender
-    {
-        NotSpecified = 0,
-        Male = 1,
-        Female = 2
-    }
-}
