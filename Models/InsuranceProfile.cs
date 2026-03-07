44790ff6bbc9244ff34ab6fbf3602d04b31834da
Nariman Mohamed <116460166+nariman26@users.noreply.github.com>
2026-03-07 16:53:47 +0200

    Add files via upload

---
+++ Models/InsuranceProfile.cs
@@ -0,0 +1,20 @@
+﻿using Salamaty.API.Models.ProfileModels;
+using SalamatyAPI.Models;
+
+public class InsuranceProfile
+{
+    public int Id { get; set; }
+
+    // تأكدي أن الـ UserId نوعه string
+    public string UserId { get; set; } = null!;
+
+    // التعديل المهم: الربط بـ ApplicationUser
+    public virtual ApplicationUser User { get; set; } = null!;
+
+    public int InsuranceProviderId { get; set; }
+    public virtual InsuranceProvider InsuranceProvider { get; set; } = null!;
+
+    public string CardHolderId { get; set; } = null!;
+    public string FrontImagePath { get; set; } = null!;
+    public string BackImagePath { get; set; } = null!;
+}
---
+++ Models/InsuranceProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace SalamatyAPI.Models
+{
+    public class InsuranceProvider
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        public string? LogoUrl { get; set; }
+        //public string? Description { get; set; }
+
+        public ICollection<InsuranceProfile> InsuranceProfiles { get; set; } =
+            new List<InsuranceProfile>();
+
+        public ICollection<InsuranceNetworkService> NetworkServices { get; set; } =
+            new List<InsuranceNetworkService>();
+    }
+}
