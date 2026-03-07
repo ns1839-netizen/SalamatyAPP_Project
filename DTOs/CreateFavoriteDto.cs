8ffd140387ad5034822d371a96fd1bb6da637d15
Nariman Mohamed <116460166+nariman26@users.noreply.github.com>
2026-03-07 16:40:51 +0200

    Delete DTOs directory

--- DTOs/CreateFavoriteDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SalamatyAPI.Dtos.Favorites
-{
-    public class CreateFavoriteDto
-    {
-        public int UserId { get; set; }
-        public int ProductId { get; set; }
-    }
-}
--- DTOs/ProfileDTOS/LocationUpdateDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Salamaty.API.DTOs.ProfileDTOS
-{
-    public class LocationUpdateDto
-    {
-        public double LocationLat { get; set; }
-        public double LocationLng { get; set; }
-    }
-}
