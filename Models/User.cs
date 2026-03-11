d2de7c5f877a8165dffd82bc57d1d137c8c6ec30
Nariman Mohamed <116460166+nariman26@users.noreply.github.com>
2026-03-11 21:11:35 +0200

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace SalamatyAPI.Models
+{
+    public class User
+    {
+       
+            public string Id { get; set; }
+            public string FullName { get; set; } = null!;
+            public string Email { get; set; } = null!;
+
+            public ICollection<InsuranceProfile> InsuranceProfiles { get; set; } = new List<InsuranceProfile>();
+        
+    }
+}
