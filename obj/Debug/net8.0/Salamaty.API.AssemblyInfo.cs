97b63d60ce5863b0647b2b8e10b7db73b4f7634c
ns1839-netizen <ns1839@fayoum.edu.eg>
2026-05-05 20:52:51 +0300

    changes

--- obj/Debug/net8.0/Salamaty.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Salamaty.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Salamaty.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b3f563a931c0a1427ff612f8067177a2298b9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae74e7e028d17c8317cb1514fc5b5f7659ce6c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Salamaty.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Salamaty.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
